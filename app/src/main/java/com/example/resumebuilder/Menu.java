9175891b03539aa624458c10d60908283ac1d851
aanch-343 <aanchalnshetty111@gmail.com>
2023-06-01 21:14:51 +0530

    ResumeBuilder App

---
+++ app/src/main/java/com/example/resumebuilder/Menu.java
@@ -0,0 +1,21 @@
+package com.example.resumebuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+
+
+
+
+    }
+}
---
+++ app/src/main/java/com/example/resumebuilder/Template3.java
@@ -0,0 +1,14 @@
+package com.example.resumebuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Template3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_template3);
+    }
+}
