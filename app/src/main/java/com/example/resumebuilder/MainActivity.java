81ac7f15e2f306b3328bf4ac6d9205e26478b37e
divyata-j <divyata271@gmail.com>
2023-06-04 18:53:33 +0530

    Linked create resume with menu

--- app/src/main/java/com/example/resumebuilder/MainActivity.java
+++ app/src/main/java/com/example/resumebuilder/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.resumebuilder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button create;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        create=(Button)findViewById(R.id.create);
+
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i1= new Intent(MainActivity.this,Menu.class);
+                startActivity(i1);
+            }
+        });
     }
 }
